7ba9a0ea2a5856eb9de589d0ef7de302e0558800
Himanshu sharma <hs991009@gmail.com>
2024-12-23 14:12:10 +0530

    404 added

--- src/App.tsx
+++ src/App.tsx
@@ -20,30 +20,30 @@ const App: React.FC = () => {
         <Navbar />
         <main className="flex-grow">
           <Routes>
-            <>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/packages/:id" element={<PackageDetails />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/admin" element={<ProtectRoute admin><AdminPanel></AdminPanel></ProtectRoute>} />
-              <Route path="/login" element={<LoginForm></LoginForm>} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/profile" element={<ProtectRoute><Profile></Profile></ProtectRoute>} />
-              <Route path="/admin/login" element={<AdminLogin />} />
-              <Route path="/forget-password" element={<ForgetPassword />} />
-              <Route path="*" element={<section className="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
+            <Route path="/" element={<HomePage />} />
+            <Route path="/packages/:id" element={<PackageDetails />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin" element={<ProtectRoute><AdminPanel /></ProtectRoute>} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<ProtectRoute><Profile /></ProtectRoute>} />
+            <Route path="/forget-password" element={<ForgetPassword />} />
+            <Route path="*" element={
+              <section className="flex items-center h-full p-16 bg-gray-50 text-gray-800">
                 <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
                   <div className="max-w-md text-center">
-                    <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-400">
+                    <h2 className="mb-8 font-extrabold text-9xl text-gray-400">
                       <span className="sr-only">Error</span>404
                     </h2>
                     <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-                    <p className="mt-4 mb-8 dark:text-gray-600">But dont worry, you can find plenty of other things on our homepage.</p>
-                    <Link to="/" className="px-8 py-3 font-semibold rounded dark:bg-gray-900 dark:text-gray-50">Back to homepage</Link>
+                    <p className="mt-4 mb-8 text-gray-600">But don't worry, you can find plenty of other things on our homepage.</p>
+                    <Link to="/" className="px-8 py-3 font-semibold rounded bg-gray-900 text-gray-50">Back to homepage</Link>
                   </div>
                 </div>
-              </section>} />
-            </>
+              </section>
+            } />
           </Routes>
         </main>
         <Footer />
